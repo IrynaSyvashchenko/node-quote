ad0682735091e000636cf6cd480ff5c8b75968b1
IrynaSyvashchenko <128808245+IrynaSyvashchenko@users.noreply.github.com>
2023-08-27 16:04:20 +0000

    Fixed test error

--- quotes-react-app/src/Utils.js
+++ quotes-react-app/src/Utils.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const generateNewQuote = (func) => {
     fetch(`https://friendly-space-tribble-5wg6jqq7r6q27rgx-5000.app.github.dev/quotes/random`)
       .then(res => res.json())
